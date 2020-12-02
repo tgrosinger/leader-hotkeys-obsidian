81c63d9e5846ebda4fcca365bb13b070aeba7952
Tony Grosinger <tony@grosinger.net>
2020-12-01 20:29:08 -0800

    eslint fixes

--- src/main.ts
+++ src/main.ts
@@ -22,10 +22,10 @@ const defaultHotkeys: Hotkey[] = [
 ];
 
 export default class LeaderHotkeysPlugin extends Plugin {
+  public settings: Settings;
+
   private leaderPending: boolean;
   private cmEditors: CodeMirror.Editor[];
-
-  public settings: Settings;
 
   public async onload(): Promise<void> {
     this.settings = (await this.loadData()) || { hotkeys: defaultHotkeys };
@@ -105,102 +105,6 @@ class LeaderPluginSettingsTab extends PluginSettingTab {
     this.currentLeader = this.lookupCurrentLeader(app);
   }
 
-  private lookupCurrentLeader = (app: App): string => {
-    for (const [key, value] of Object.entries((app as any).commands.commands)) {
-      if (key !== 'leader-hotkeys-obsidian:leader') {
-        continue;
-      }
-
-      return value.hotkeys
-        .map(
-          (hotkey: any): string =>
-            hotkey.modifiers.join('+') + '+' + hotkey.key,
-        )
-        .join(' or ');
-    }
-  };
-
-  private generateCommandList = (app: App): Command[] => {
-    const commands: Command[] = [];
-    for (const [key, value] of Object.entries((app as any).commands.commands)) {
-      commands.push({ name: value.name, id: value.id });
-    }
-    return commands;
-  };
-
-  private validateNewHotkey = (value: string): boolean => {
-    console.log(`Validating value: '${value}'`);
-    if (value.length !== 1) {
-      new Notice('Leader hotkeys may only be a single letter');
-      return false;
-    }
-
-    for (let i = 0; i < this.plugin.settings.hotkeys.length; i++) {
-      if (this.plugin.settings.hotkeys[i].key === value) {
-        new Notice(`Leader hotkey '${value}' is already in use`);
-        return false;
-      }
-    }
-
-    return true;
-  };
-
-  private deleteHotkeyFromSettings = (key: string) => {
-    for (let i = 0; i < this.plugin.settings.hotkeys.length; i++) {
-      const hotkey = this.plugin.settings.hotkeys[i];
-      if (hotkey.key !== key) {
-        continue;
-      }
-
-      console.log(`Removing leader-hotkey ${key} at index ${i}`);
-      this.plugin.settings.hotkeys.splice(i, 1);
-    }
-    this.plugin.saveData(this.plugin.settings);
-  };
-
-  private updateHotkeyInSettings = (key: string, newKey: string) => {
-    for (let i = 0; i < this.plugin.settings.hotkeys.length; i++) {
-      const hotkey = this.plugin.settings.hotkeys[i];
-      if (hotkey.key !== key) {
-        continue;
-      }
-
-      console.log(`Updating leader-hotkey ${key} at index ${i} to ${newKey}`);
-      hotkey.key = newKey;
-      break;
-    }
-    this.plugin.saveData(this.plugin.settings);
-  };
-
-  private updateHotkeyCommandInSettings = (key: string, newCommand: string) => {
-    for (let i = 0; i < this.plugin.settings.hotkeys.length; i++) {
-      const hotkey = this.plugin.settings.hotkeys[i];
-      if (hotkey.key !== key) {
-        continue;
-      }
-
-      console.log(
-        `Updating leader-hotkey command ${key} at index ${i} to ${newCommand}`,
-      );
-      hotkey.commandID = newCommand;
-      break;
-    }
-    this.plugin.saveData(this.plugin.settings);
-  };
-
-  private storeNewHotkeyInSettings = () => {
-    console.log(
-      `Adding leader-hotkey command ${this.tempNewHotkey} to ${this.tempNewCommand}`,
-    );
-    this.plugin.settings.hotkeys.push({
-      key: this.tempNewHotkey,
-      commandID: this.tempNewCommand,
-    });
-    this.plugin.saveData(this.plugin.settings);
-    this.tempNewHotkey = '';
-    this.tempNewCommand = '';
-  };
-
   public display(): void {
     const { containerEl } = this;
     containerEl.empty();
@@ -218,8 +122,8 @@ class LeaderPluginSettingsTab extends PluginSettingTab {
 
     containerEl.createEl('h3', { text: 'Existing Hotkeys' });
 
-    this.plugin.settings.hotkeys.forEach((command) => {
-      const { key, commandID } = command;
+    this.plugin.settings.hotkeys.forEach((configuredCommand) => {
+      const { key, commandID } = configuredCommand;
 
       new Setting(containerEl)
         .addExtraButton((button) => {
@@ -291,4 +195,106 @@ class LeaderPluginSettingsTab extends PluginSettingTab {
       });
     });
   }
+
+  private readonly lookupCurrentLeader = (app: App): string => {
+    for (const [key, value] of Object.entries((app as any).commands.commands)) {
+      if (key !== 'leader-hotkeys-obsidian:leader') {
+        continue;
+      }
+
+      return value.hotkeys
+        .map(
+          (hotkey: any): string =>
+            hotkey.modifiers.join('+') + '+' + hotkey.key,
+        )
+        .join(' or ');
+    }
+  };
+
+  private readonly generateCommandList = (app: App): Command[] => {
+    const commands: Command[] = [];
+    for (const [key, value] of Object.entries((app as any).commands.commands)) {
+      commands.push({ name: value.name, id: value.id });
+    }
+    return commands;
+  };
+
+  private readonly validateNewHotkey = (value: string): boolean => {
+    console.log(`Validating value: '${value}'`);
+    if (value.length !== 1) {
+      new Notice('Leader hotkeys may only be a single letter');
+      return false;
+    }
+
+    for (let i = 0; i < this.plugin.settings.hotkeys.length; i++) {
+      if (this.plugin.settings.hotkeys[i].key === value) {
+        new Notice(`Leader hotkey '${value}' is already in use`);
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  private readonly deleteHotkeyFromSettings = (key: string): void => {
+    for (let i = 0; i < this.plugin.settings.hotkeys.length; i++) {
+      const hotkey = this.plugin.settings.hotkeys[i];
+      if (hotkey.key !== key) {
+        continue;
+      }
+
+      console.log(`Removing leader-hotkey ${key} at index ${i}`);
+      this.plugin.settings.hotkeys.splice(i, 1);
+    }
+    this.plugin.saveData(this.plugin.settings);
+  };
+
+  private readonly updateHotkeyInSettings = (
+    key: string,
+    newKey: string,
+  ): void => {
+    for (let i = 0; i < this.plugin.settings.hotkeys.length; i++) {
+      const hotkey = this.plugin.settings.hotkeys[i];
+      if (hotkey.key !== key) {
+        continue;
+      }
+
+      console.log(`Updating leader-hotkey ${key} at index ${i} to ${newKey}`);
+      hotkey.key = newKey;
+      break;
+    }
+    this.plugin.saveData(this.plugin.settings);
+  };
+
+  private readonly updateHotkeyCommandInSettings = (
+    key: string,
+    newCommand: string,
+  ): void => {
+    for (let i = 0; i < this.plugin.settings.hotkeys.length; i++) {
+      const hotkey = this.plugin.settings.hotkeys[i];
+      if (hotkey.key !== key) {
+        continue;
+      }
+
+      console.log(
+        `Updating leader-hotkey command ${key} at index ${i} to ${newCommand}`,
+      );
+      hotkey.commandID = newCommand;
+      break;
+    }
+    this.plugin.saveData(this.plugin.settings);
+  };
+
+  private readonly storeNewHotkeyInSettings = (): void => {
+    console.log(
+      `Adding leader-hotkey command ${this.tempNewHotkey} to ${this.tempNewCommand}`,
+    );
+    this.plugin.settings.hotkeys.push({
+      key: this.tempNewHotkey,
+      commandID: this.tempNewCommand,
+    });
+    this.plugin.saveData(this.plugin.settings);
+    this.tempNewHotkey = '';
+    this.tempNewCommand = '';
+  };
 }
